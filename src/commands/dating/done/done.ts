fabfb969ff41a2f409252d7c82c1ebce0f542a39
ipmanlk <io@navinda.xyz>
2021-05-26 20:19:57 +0530

    add: done command

---
+++ src/commands/dating/done/done.ts
@@ -0,0 +1,165 @@
+import { Message, MessageReaction, User } from "discord.js";
+import { deleteMatch, getMatch } from "../../../data";
+import { getBotConfig } from "../../../util/config";
+import { parseYAML } from "../../../util/parse";
+
+const botConfig: any = getBotConfig();
+const cmdConfig: any = parseYAML(`${__dirname}/done.yaml`);
+
+export const handle = async (msg: Message) => {
+	if (msg.channel.type != "text") {
+		return;
+	}
+
+	if (msg.channel.parentID != cmdConfig["DATING CATEGORY"]) {
+		return;
+	}
+
+	const guild = msg.guild;
+	const matchChannel = msg.channel;
+
+	msg.delete({ timeout: 5000 });
+
+	// get dating channel info
+	const res = await getMatch(msg.channel.id);
+
+	if (res.isErr()) {
+		console.log("Unable to access the database.");
+		return;
+	}
+
+	const record = res.value;
+
+	const authorMember = await guild?.members.fetch(record.authorId);
+	const matchMember = await guild?.members.fetch(record.matchId);
+
+	if (!authorMember) {
+		console.log("Unable to find the author");
+		return;
+	}
+
+	if (!matchMember) {
+		console.log("Unable to find the matched member");
+		return;
+	}
+
+	const doneConfirmation = await matchChannel.send(
+		`<@${authorMember.id}> <@${matchMember.id}>,`,
+		{
+			embed: {
+				title: "Private channel is about to close.",
+				color: 0xff007f,
+				description: `**You won't be able to access or recover this chat after the confirmation.**`,
+				footer: {
+					text: `Please react with 👍 to accept or 👎 to confirm.`,
+				},
+			},
+		}
+	);
+
+	await doneConfirmation.react("👍");
+	await doneConfirmation.react("👎");
+
+	let collected;
+	try {
+		const filter = (reaction: MessageReaction, user: User) => {
+			return (
+				["👍", "👎"].includes(reaction.emoji.name) &&
+				[authorMember.id, matchMember.id].includes(user.id)
+			);
+		};
+
+		collected = await doneConfirmation.awaitReactions(filter, {
+			max: 2,
+			time: 60000,
+			errors: ["time"],
+		});
+	} catch (e) {
+		matchChannel.send({
+			embed: {
+				color: 0xff0000,
+				description:
+					"Channel closing failed because a parties failed to respond.",
+			},
+		});
+		return;
+	}
+
+	let authorConfirmed = false;
+	let matchConfirmed = false;
+
+	collected.forEach((r) => {
+		if (!authorConfirmed) {
+			authorConfirmed = r.users.cache.get(authorMember.id) != undefined;
+		}
+		if (!matchConfirmed) {
+			matchConfirmed = r.users.cache.get(matchMember.id) != undefined;
+		}
+	});
+
+	if (authorConfirmed && matchConfirmed == false) {
+		matchChannel.send({
+			embed: {
+				color: 0xff0000,
+				description:
+					"Channel closing failed because a parties failed to respond.",
+			},
+		});
+		return;
+	}
+
+	const deleteRes = await deleteMatch(matchChannel.id);
+
+	if (deleteRes.isErr()) {
+		matchChannel.send({
+			embed: {
+				color: 0xff0000,
+				description: "Unable to close this private channel.",
+			},
+		});
+		return;
+	}
+
+	await matchChannel.send({
+		embed: {
+			color: 0x2d91e7,
+			description:
+				"**Closing has been confirmed. This private channel will close in few moments.**",
+		},
+	});
+
+	setTimeout(() => {
+		matchChannel.delete().catch((e) => console.log(e));
+	}, 10000);
+
+	sendDualResponse(
+		authorMember.user,
+		matchMember.user,
+		`Your private chat with **${
+			authorMember.nickname || authorMember.user.username
+		}** has been closed.`,
+		`Your private chat with **${
+			matchMember.nickname || matchMember.user.username
+		}** has been closed.`
+	);
+};
+
+const sendDualResponse = (
+	author: User,
+	match: User,
+	authorMsg: string,
+	matchMsg: string
+) => {
+	author.send({
+		embed: {
+			color: 0x2d91e7,
+			description: authorMsg,
+		},
+	});
+	match.send({
+		embed: {
+			color: 0x2d91e7,
+			description: matchMsg,
+		},
+	});
+};
