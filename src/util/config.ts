9ed259d364156df7605f9e5af3efd897072d848c
ipmanlk <io@navinda.xyz>
2021-05-26 20:19:48 +0530

    add: reaction role names for genders

--- src/util/config.ts
+++ src/util/config.ts
@@ -3,3 +3,9 @@ import { parseYAML } from "./parse";
 export const getBotConfig = () => {
 	return parseYAML(`${__dirname}/../../setup/config.yaml`);
 };
+
+export const getReactionRoleConfig = () => {
+	return parseYAML(
+		`${__dirname}/../non_commands/reaction_role/reaction_role.yaml`
+	);
+};
